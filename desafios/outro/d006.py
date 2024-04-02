80c339738643406c657e909f0ae33b778326bbd5
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-04-02 20:14:34 -0300

    Dois desafios criados, um deles em processo.

---
+++ desafios/outro/d006.py
@@ -0,0 +1,9 @@
+metros = float(input('Uma distância em metros: '))
+km = metros / 1000
+hm = metros / 100
+dam = metros / 10
+dm = metros * 10
+cm = metros * 100
+mm = metros * 1000
+print('A medida de {}m corresponde a: \n{}\n{}\n{}\n{}\n{}\n{}'.format(metros, km, hm, dam, dm, cm, mm))
+
