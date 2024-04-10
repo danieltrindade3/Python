0d611673ba793cb10539bce9beac31ba55c872bb
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-04-10 08:25:49 -0300

    Desafio da tabuada criada e do conversor de metros modificado.

--- desafios/outro/d006.py
+++ desafios/outro/d006.py
@@ -5,5 +5,5 @@ dam = metros / 10
 dm = metros * 10
 cm = metros * 100
 mm = metros * 1000
-print('A medida de {}m corresponde a: \n{}\n{}\n{}\n{}\n{}\n{}'.format(metros, km, hm, dam, dm, cm, mm))
+print('A medida de {}m corresponde a: \n{:.1f}km\n{:.1f}hm\n{:.1f}dam\n{:.1f}dm\n{:.1f}cm\n{:.1f}mm'.format(metros, km, hm, dam, dm, cm, mm))
 
