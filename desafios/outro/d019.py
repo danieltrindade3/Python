03faf3105106c1c4673df48ff7d478993a6a5cfe
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-07-13 08:45:20 -0300

    Desafio de análise de nome criado.

---
+++ desafios/outro/d019.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite o seu nome completo: ')).strip()
+nome2 = nome.split()
+print('Analisando o seu nome...')
+print('Seu nome em maiúsculas é {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+# print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+print('Seu primeiro nome é {} e ele tem {} letras'.format(nome2[0], len(nome2[0])))
--- exercícios/manipulação-texto/ex005.py
+++ exercícios/manipulação-texto/ex005.py
@@ -1,4 +1,5 @@
 nome = 'Daniel Trindade Barbosa'
 dividido = nome.split()
 print(dividido)
+print('-'.join(dividido))
 print(dividido[0] [4])
