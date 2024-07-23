1b751b2d9ed24ed7a398d93b3258458b7ca16dba
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-07-23 12:34:05 -0300

    Desafio de criar computador que pensa em um número para adivinhar.

---
+++ desafios/curso-em-video/d025.py
@@ -0,0 +1,13 @@
+from random import randint
+from time import sleep
+com = randint(0, 5) # Faz o computador "pensar"
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+num = int(input('Em que número eu pensei? '))
+print('Processando...')
+sleep(3)
+if num == com:
+    print('Parabéns! Você conseguiu me vencer!')
+else:
+    print('Ganhei! Eu pensei em {} ao invés de {}'.format(com, num))
